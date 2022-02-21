36cf7bfb42169e1380d0681d7c5a9ff8565c7265
dogusakkas <dogusakkas@gmail.com>
2022-02-21 18:27:18 +0300

    PageList yapısı ve CategoryValidator oluşturulması

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adı boş geçilemez").Length(2,50).WithMessage("Kategori adı en az 2, en fazla 50 karakter olabilir.");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Kategori açıklaması boş geçilemez");
+        }
+    }
+}
