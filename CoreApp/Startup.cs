de3e89e77ca969ba49f0bfefb40beb068ce43467
dogusakkas <dogusakkas@gmail.com>
2022-02-28 15:45:30 +0300

    Apiler ile projenin haberleşmesi

--- CoreApp/Startup.cs
+++ CoreApp/Startup.cs
@@ -73,12 +73,12 @@ namespace CoreApp
             {
                 endpoints.MapControllerRoute(
             name: "areas",
-            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-          );
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
             });
         }
     }
