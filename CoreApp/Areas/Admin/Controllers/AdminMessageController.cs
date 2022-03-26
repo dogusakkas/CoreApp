c5f7803063ff47bacec724da8171ac5b7b834d06
dogusakkas <dogusakkas@gmail.com>
2022-03-26 14:22:16 +0300

    Mesaj Gönderme işlemi yapıldı. Sendbox'daki hata çözüldü

--- CoreApp/Areas/Admin/Controllers/AdminMessageController.cs
+++ CoreApp/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using CoreApp.Models;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -29,12 +31,27 @@ namespace CoreApp.Areas.Admin.Controllers
             var username = User.Identity.Name;
             var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
-            var values = m2m.GetInboxListByWriter(writerID);
+            var values = m2m.GetSendboxListByWriter(writerID);
             return View(values);
         }
+        [HttpGet]
         public IActionResult ComposeMessage()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult ComposeMessage(Message2 p)
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            p.SenderID = writerID;
+            p.ReceiverID = 2;
+            p.MessageDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.MessageStatus = true;
+            m2m.TAdd(p);
+            return RedirectToAction("SendBox");
         }
     }
 }
